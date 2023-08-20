41e16cf320d7803fec1442011b0948b0388c222c
Rafal Czyzewski <rafal.czyzewski@gmail.com>
2023-08-20 11:10:59 +0200

    compilation a file from resources

--- guacamole-om/src/test/java/io/github/rczyzewski/guacamole/ddb/processor/DynamoDBProcessorTest.java
+++ guacamole-om/src/test/java/io/github/rczyzewski/guacamole/ddb/processor/DynamoDBProcessorTest.java
@@ -1,5 +1,7 @@
 package io.github.rczyzewski.guacamole.ddb.processor;
 
+import com.google.testing.compile.Compilation;
+import com.google.testing.compile.JavaFileObjects;
 import io.github.rczyzewski.guacamole.ddb.processor.generator.IndexSelectorGenerator;
 import io.github.rczyzewski.guacamole.ddb.processor.generator.LiveDescriptionGenerator;
 import io.github.rczyzewski.guacamole.ddb.processor.generator.LogicalExpressionBuilderGenerator;
@@ -8,24 +10,32 @@ import io.github.rczyzewski.guacamole.ddb.processor.model.ClassDescription;
 import io.github.rczyzewski.guacamole.ddb.processor.model.DDBType;
 import io.github.rczyzewski.guacamole.ddb.processor.model.FieldDescription;
 import io.github.rczyzewski.guacamole.processor.NormalLogger;
+import lombok.Lombok;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import javax.annotation.processing.Processor;
+import javax.tools.JavaFileObject;
 import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStreamWriter;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static com.google.testing.compile.CompilationSubject.assertThat;
+import static com.google.testing.compile.Compiler.javac;
+
 @Slf4j
-class DynamoDBProcessorTest{
+class DynamoDBProcessorTest {
 
 
     @Test
     @SneakyThrows
-    void sunnyDayTest(){
+    void sunnyDayTest() {
         NormalLogger logger = new NormalLogger();
         List<FieldDescription> fields = new ArrayList<>();
         fields.add(FieldDescription.builder().isHashKey(true).attribute("ID").typeName("java.lang.String").ddbType(DDBType.STRING).name("ABC").build());
@@ -45,11 +55,39 @@ class DynamoDBProcessorTest{
                 .logger(logger)
                 .build();
         ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
-        BufferedWriter bw =  new BufferedWriter(new OutputStreamWriter(byteArray));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(byteArray));
         a.generateRepositoryCode(classDescription).writeTo(bw);
         bw.close();
-       log.info("code: {}",  byteArray);
+        log.info("code: {}", byteArray);
 
 
+    }
+
+    @Test
+    @SneakyThrows
+    void compilationHappyDay() throws ClassNotFoundException {
+        //https://github.com/google/compile-testing/issues/329
+        //lombok is required to compile it
+
+        JavaFileObject entity = JavaFileObjects.forResource("io/github/rczyzewski/guacamole/tests/FakeCountry.java");
+
+
+        Class<?> lombokAnnotationProcessor = getClass().getClassLoader().loadClass("lombok.launch.AnnotationProcessorHider$AnnotationProcessor");
+        Class<?> lombokClaimingProcessor = getClass().getClassLoader().loadClass("lombok.launch.AnnotationProcessorHider$ClaimingProcessor");
+
+        Compilation compilation = javac()
+                .withProcessors(
+                        (Processor) lombokAnnotationProcessor.getDeclaredConstructor().newInstance(),
+                        (Processor) lombokClaimingProcessor.getDeclaredConstructor().newInstance(),
+                        new DynamoDBProcessor())
+                .compile(entity);
+
+        assertThat(compilation).succeeded();
+          /*
+          assertThat(compilation)
+                           .hadErrorContaining("No types named HelloWorld!")
+                           .inFile(helloWorld)
+                            .onLine(23)
+                            .atColumn(5); */
     }
 }
